6e6f1f6c2a8b8d5222d819655ddb222c250dcaba
Alberto Becerra <alberto.becerra.tome@gmail.com>
2023-03-07 00:49:02 +0100

    Version calling shoot method

--- main.py
+++ main.py
@@ -14,69 +14,13 @@ print(player_alb)
 print(player_comp)
 
 while True:
-    while True:
-        print(f"Turno de {player_alb.name}\n")
-
-        while True:
-            player_x = int(input("Introduzca coordenada X: "))
-            player_y = int(input("Introduzca coordenada Y: "))
-
-            if tab_comp.check_coordinates(player_x, player_y):
-                break
-            else:
-                print("Introduzca coordenadas dentro del tablero y en las que no se haya disparado ya")
-
-        print(f"Ataque en {(player_x, player_y)}!!\n")
-
-        sleep(3)
-
-        player_comp.receive_shot(player_x, player_y)
-
-        print(player_alb)
-        print(player_comp)
-
-        if player_comp.impacted:
-            if np.sum(player_comp.tablero.tablero == HIT_SIGN) == sum(BOAT_SIZES):
-                break
-            else:
-                print(f"TOCADO!! {player_alb.name.upper()} DISPARA DE NUEVO\n")
-                continue
-        else:
-            print("AGUA!!")
-            break
-    
-    if np.sum(player_comp.tablero.tablero == HIT_SIGN) == sum(BOAT_SIZES):
+    player_alb.shoot(player_comp)
+    if player_comp.finished:
         print(f"FIN DEL JUEGO. {player_alb.name.upper()} GANA")
         break
 
-    while True:
-        print(f"Turno de {player_comp.name}\n")
-        while True:
-            comp_x, comp_y = np.random.randint(0, BOARD_SIZE, 2)
-
-            if tab_jugador.check_coordinates(comp_x, comp_y):
-                break
-
-        print(f"Ataque en {(comp_x, comp_y)}!!\n")
-        
-        sleep(3)
-
-        player_alb.receive_shot(comp_x, comp_y)
-
-        print(player_alb)
-        print(player_comp)
-
-        if player_alb.impacted:
-            
-            if np.sum(player_alb.tablero.tablero == HIT_SIGN) == sum(BOAT_SIZES):
-                break
-            else:
-                print(f"TOCADO!! {player_comp.name.upper()} DISPARA DE NUEVO\n")
-                continue
-        else:
-            break
-    
-    if np.sum(player_alb.tablero.tablero == HIT_SIGN) == sum(BOAT_SIZES):
+    player_comp.shoot(player_alb)
+    if player_alb.finished:
         print(f"FIN DEL JUEGO. {player_comp.name.upper()} GANA")
         break
 
